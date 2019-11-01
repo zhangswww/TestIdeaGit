ce9bdd5ce670d0e9d23738917fcebd6afc0bd0c4
hello <hello@qq.com>
2019-11-01 18:15:04 +0800

    测试提交到本地仓库

---
+++ src/main/java/com/example/TestGit.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * @author:zhang
+ * @description: date 2019/11/1
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("zhangsan");
+        System.out.println("zhangsan");
+        System.out.println("zhangsan");
+        System.out.println("lisi");
+        System.out.println("lisi");
+        System.out.println("lisi");
+    }
+}
